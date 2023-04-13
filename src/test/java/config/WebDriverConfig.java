43425c70f3ef18114790cc06fb29eb4fe1bdf348
Maria Ryabova <mvryabova2@gmail.com>
2023-04-13 17:38:09 +0300

    Tests modified

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -9,6 +9,7 @@ import org.aeonbits.owner.Config;
 public interface WebDriverConfig extends Config {
 
     @Key("launchType")
+    @DefaultValue("remote")
     String getLaunchType();
 
     @Key("browser")
--- src/test/java/pages/ProfileInfo.java
+++ src/test/java/pages/ProfileInfo.java
@@ -3,7 +3,6 @@ package pages;
 import test.stepik.TestBase;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProfileInfo extends TestBase {
@@ -25,11 +24,7 @@ public class ProfileInfo extends TestBase {
 
     public ProfileInfo confirmChanges() {
         $(" .modal-dialog-confirm__buttons").shouldBe(visible);
-        if ($(" .modal-dialog-confirm__buttons").$(byText("Сохранить")).is(visible)) {
-            $(" .modal-dialog-confirm__buttons").$(byText("Сохранить")).click();
-        } else {
-            $(" .modal-dialog-confirm__buttons").$(byText("Save")).click();
-        }
+        $(" .modal-dialog-confirm__buttons [type=button]").click();
         return this;
     }
 }
